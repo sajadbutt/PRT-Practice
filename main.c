22f29413d228fe780693e7b8bfbe7c8816a50897
Sameer <109897419+Sameer-8080@users.noreply.github.com>
2023-10-17 19:26:38 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int number = 5;
+    if (number > 0) {
+        printf("The number is positive.\n");
+    } else {
+        printf("The number is non-positive.\n");
+    }
+    return 0;
+}
